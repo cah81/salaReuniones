ec23788779f3f64bd47462c4a0d3da21ba535d47
cah81 <carlosenriqueanguloherrera@gmail.com>
2025-11-09 09:02:47 +0100

    Primera subida prueba clase Reunion y Model con tymleaf

---
+++ src/main/java/com/app/meet/MeetApplication.java
@@ -0,0 +1,13 @@
+package com.app.meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/meet/models/Reunion.java
@@ -0,0 +1,53 @@
+package com.app.meet.models;
+
+import java.time.ZonedDateTime;
+
+public class Reunion {
+    private long id;
+    private String asunto;
+    private ZonedDateTime fecha;
+
+    public Reunion() {
+    }
+
+    public Reunion(long id, String asunto, ZonedDateTime fecha) {
+        this.id = id;
+        this.asunto = asunto;
+        this.fecha = fecha;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public ZonedDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(ZonedDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Reunion{" +
+                "id=" + id +
+                ", asunto='" + asunto + '\'' +
+                ", fecha=" + fecha +
+                '}';
+    }
+}
+
+
