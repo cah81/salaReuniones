efb1570b492dc3d9f64daa1dbaf5aad1699989da
cah81 <carlosenriqueanguloherrera@gmail.com>
2025-11-11 13:14:05 +0100

    se agrega repositorio de reuniones

---
+++ src/main/java/com/app/meet/data/ReunionRepository.java
@@ -0,0 +1,8 @@
+package com.app.meet.data;
+
+import com.app.meet.models.Reunion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReunionRepository extends JpaRepository<Reunion,Long> {
+
+}
